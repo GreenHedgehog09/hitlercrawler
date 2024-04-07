816e94f9f0fb18855353dbd8a49453e3e2d0457e
Sergii Bida <sergijbida15@gmail.com>
2024-04-07 19:25:18 +0300

    did multiprocessing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import multiprocessing
 import re
 import time
 
@@ -7,7 +8,7 @@ from bs4 import BeautifulSoup
 
 
 class HitlerLinksSearch:
-    async def request(self, session, url):
+    async def request(self, session, url: str) -> list:
         async with session.get(url) as response:
             body = await response.text()
             soup = BeautifulSoup(body, 'lxml')  # Parser lxml(fast)
@@ -21,21 +22,17 @@ class HitlerLinksSearch:
                     links[i] = 'https://en.wikipedia.org/' + link
             return [False, links]
 
-    async def create_tasks(self, urls):
+    async def create_tasks(self, urls: list) -> tuple:
         async with aiohttp.ClientSession() as session:
             tasks = [asyncio.ensure_future(self.request(session, url)) for url in urls]
             responses = await asyncio.gather(*tasks)
             return responses
 
-    def main(self, link):
-        output_link = 'Hitler`s page not found...'
-        urls = []
-        layer = 0
-        while layer < 7:
-            if not layer:
-                urls = [link]
-            else:
-                print(f'Pass layer: {layer}')
+    def main_process(self, urls: list) -> str:
+        """Start new process"""
+        default = ''
+        count = 0
+        while count < 6:
             # Start tasks
             loop = asyncio.get_event_loop()
             responses = loop.run_until_complete(self.create_tasks(urls))
@@ -45,8 +42,26 @@ class HitlerLinksSearch:
                     return response[1]
                 else:
                     urls.extend(response[1])
-            layer += 1
-        return output_link
+            count += 1
+        return default
+
+    def primary_search(self, link: str) -> list:
+        urls = []
+        # Start task
+        loop = asyncio.get_event_loop()
+        responses = loop.run_until_complete(self.create_tasks([link]))
+        # Check
+        for response in responses:
+            if response[0]:
+                return [True, response[1]]
+            else:
+                urls.extend(response[1])
+        return [False, urls]
+
+
+def run_processes(obj, method_name, urls):
+    method = getattr(obj, method_name)
+    return method(urls)
 
 
 if __name__ == '__main__':
@@ -55,8 +70,24 @@ if __name__ == '__main__':
         if input_link == '':
             print('Exit...')
             break
+
+        # Search
         start_time = time.time()
         obj_hitler = HitlerLinksSearch()
-        result = obj_hitler.main(input_link)
-        print(f'Result: {result}')
+        first_result = obj_hitler.primary_search(link=input_link)
+        if first_result[0]:
+            print(f'Result: {first_result[1]}')
+        else:
+            # Data preparation
+            urls = first_result[1]
+            nested_list = [urls[:len(urls)//2], urls[len(urls)//2:]]
+            # Start 2 processes
+            with multiprocessing.Pool(processes=2) as pool:
+                results = pool.starmap(run_processes, [(obj_hitler, 'main_process', urls) for urls in nested_list])
+            for result in results:
+                if result:
+                    print(f'Result: {result}')
+                    break
+            else:
+                print(f'Result: Hitler`s page not found...')
         print(f'Duration: {time.time() - start_time}')
